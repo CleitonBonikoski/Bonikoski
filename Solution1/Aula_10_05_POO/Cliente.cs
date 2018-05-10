a3e760f091e109416c9d91f71bbf6771aab712cb
Aluno <Aluno@DOSO125280>
2018-05-10 10:14:25 -0300

    Realizado Set e Get dos atributos. Criado Propriedades.

---
+++ Solution1/Aula_10_05_POO/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula_10_05_POO {
+    class Cliente {
+        //Atributo
+        private string nome;
+        // public string cpf { set; get; }
+
+        //Propriedade
+        public string Nome {
+            get {
+                return nome;
+                }
+
+            set {
+                nome = value;
+                }
+
+            }
+
+        public string cpf { set; get; } // propriedade sem declarar o atributo.
+
+        }
+    }
--- Solution1/Aula_10_05_POO/Program.cs
+++ Solution1/Aula_10_05_POO/Program.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace Aula_10_05_POO {
     class Program {
         static void Main(string[] args) {
-            }
+            Cliente cli = new Cliente();
+            cli.Nome = "Gogofredo"; // set
+            String nomeCliente = cli.Nome; //get
+            cli.cpf = "12345678909";
+            string cpfCliente = cli.cpf;
+
+            Console.WriteLine(" CPF: " + cpfCliente);
+            Console.ReadKey();
+                
+                    
+           }
         }
     }
