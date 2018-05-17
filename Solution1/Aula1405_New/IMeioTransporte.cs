2d88df369924a8bb1e7eeac976a4ecf9502f189a
Aluno <Aluno@DOSO125287>
2018-05-17 08:31:17 -0300

    Criando MVC

---
+++ Solution1/Aula1405_New/IMeioTransporte.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_Interfaces {
+    interface IMeioTransporte {
+
+        public string Modelo { get; set; }
+        int Velocidade { get; set; }
+        void Acelerar();
+        void Desacelerar();
+    }
+}
---
+++ Solution1/Aula1405_New/Roda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_Interfaces {
+    class Roda {
+
+        public int RodaID { get; set; }
+
+        public string Modelo { get; set; }
+
+        public int MarcaID { get; set; }
+
+        public Marca _Marca { get; set; }
+
+    }
+}
