b6a9eba555422d714b70e657ba83374c2fba9f9c
Aluno <Aluno@DOSO125287>
2018-05-17 09:24:42 -0300

    Criando View

---
+++ Solution1/Aula1705_Camadas/Views/AtividadesView.cs
@@ -0,0 +1,51 @@
+﻿using Aula1705_Camadas.Controllers;
+using Aula1705_Camadas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1705_Camadas.Views
+{
+    class AtividadesView
+    {
+        public int ExibirMenu()
+        {
+            Console.WriteLine("======================");
+            Console.WriteLine("Escolha uma Opção:");
+            Console.WriteLine("1 - Criar Atividade: ");
+            Console.WriteLine("2 - Listar Atividades: ");
+            Console.WriteLine("3 - Buscar Atividade: ");
+            Console.WriteLine("4 - Editar Atividade: ");
+            Console.WriteLine("5 - Excluir Atividade: ");
+            Console.WriteLine("======================");
+
+            int opcao = int.Parse(Console.ReadLine());
+            return opcao;
+            
+        }
+
+        public void CriarAtividade()
+        {
+            Atividade atividade = new Atividade();
+            Console.Write("Digite o nome da atividade: ");
+            atividade.Nome =  Console.ReadLine();
+
+            atividade.Ativo = true;
+
+            AtividadesController atividadeCtrl = new AtividadesController();
+            atividadeCtrl.Salvar(atividade);
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
